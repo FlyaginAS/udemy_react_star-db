0c23d758fa8a6b214f57153f4ec21145f1a5c693
ruscat <rus.reg.36@gmail.com>
2020-01-09 15:07:04 +0300

    53

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import './bootstrap.min.css';
 
 const getResource = async (url)=>{
    const res = await  fetch(url);
+   if (!res.ok){
+       throw  new Error('Could not fetch');
+   }
    const body = await  res.json();
    return body;
 };
-getResource('https://swapi.co/api/people/1/')
+getResource('https://swapi.co/api/people/123123/')
     .then((body)=>{
         console.log(body);
+    })
+    .catch((err)=>{
+        console.log('Could not fetch',err);
     });
