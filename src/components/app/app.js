fa536fa95c609c46c48d8e6b6b7ab8308730f8fc
ruscat <rus.reg.36@gmail.com>
2020-01-21 12:22:01 +0300

    69

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,29 +1,39 @@
 import React from 'react';
-
+import ErrorIndicator from '../error-indicator';
 import Header from '../header';
 import RandomPlanet from '../random-planet';
 import ItemList from '../item-list';
 import PersonDetails from '../person-details';
 
 import './app.css';
+import ErrorButton from "../error-button/error-button";
 
 export  default  class App extends React.Component{
     state= {
        showRandomPlanet: true,
        selectedPerson: 5,
+        hasError: false,
     };
     onPersonSelected = (id)=>{
        this.setState({
            selectedPerson: id,
        }) ;
     };
-
+    componentDidCatch(){
+        console.log('componentDidCatch');
+        this.setState({hasError: true});
+    }
     render(){
+        if(this.state.hasError) {
+            return <ErrorIndicator/>;
+        }
         return (
             <div>
                 <Header />
                 <RandomPlanet />
-
+                <div className='row mb2 button-row'>
+                    <ErrorButton/>
+                </div>
                 <div className="row mb2">
                     <div className="col-md-6">
                         <ItemList onItemSelected={this.onPersonSelected}/>
