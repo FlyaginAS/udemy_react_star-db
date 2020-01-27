10cc7f80b84fab3e8ae3bac2856065a59df8c86d
ruscat <rus.reg.36@gmail.com>
2020-01-27 13:35:33 +0300

    80

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -28,6 +28,4 @@ const ItemList = (props) => {
   );
 };
 
-const { getAllPeople } = new SwapiService();
-
-export default withData(ItemList, getAllPeople);
+export  default ItemList;
---
+++ src/components/sw-components/index.js
@@ -0,0 +1,19 @@
+import {
+    PersonDetails,
+    PlanetDetails,
+    StarshipDetails
+} from "./details";
+import {
+    PersonList,
+    StarshipList,
+    PlanetList
+} from "./item-lists";
+
+export  {
+    PersonDetails,
+    PlanetDetails,
+    StarshipDetails,
+    PersonList,
+    StarshipList,
+    PlanetList,
+}
